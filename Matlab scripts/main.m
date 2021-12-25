a9223f89aee7b603e1bcdfa64b93c7c4a27a4bab
Ramanath Amith <79580531+amithvaidya@users.noreply.github.com>
2021-12-25 20:38:21 +0530

    Update and rename mainscript.m to main.m

--- Matlab scripts/main.m
+++ Matlab scripts/main.m
@@ -2,7 +2,7 @@ clc
 clear
 marks=zeros(10);
 
-%loading excel data to marks
+%loading excel data to marks matrix
 marks = xlsread('matrix1.xlsx');
 
 %constructing histogram
@@ -12,13 +12,13 @@ end
 
 %finding th position of peak
 for i=1:6
-    peakpos(i)=iomax((hsub(i,:)));
+    peakpos(i)=index_of_max((hsub(i,:)));
 end
 
 
 %ranking the subjects based on peakpos
 for i=1:6
-    rank(i)=iomin(peakpos)
-    peakpos(iomin(peakpos))='x';
+    rank(i)=index_of_min(peakpos)
+    peakpos(index_of_min(peakpos))='x';
 end
 
